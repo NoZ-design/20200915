09e1e424479ec25e4367c5ae1d00457469eebbb2
NoZ-design <68496572+NoZ-design@users.noreply.github.com>
2020-09-17 11:04:44 +0800

    20200915

---
+++ 20200915/20200915/20200915.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int num;
+	num = 18;
+	cout << "I'm" << num << "years-old." << endl;
+	cout << "You're" << num << ",too." << endl;
+	system("pause");
+	return 0;
+}
